3e2421332690bff07314d75bfe114e389e3aeee4
Adnan Rahman <arahman4946@gmail.com>
2021-06-29 01:06:01 +0600

    Initial commit

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shop_management_system";
+
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+
+/*if($conn)
+echo "connected";*/ 
+
+
+?>
